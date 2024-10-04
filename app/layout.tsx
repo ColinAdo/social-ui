8213f7edf09460153148075610348af35f0bc13e
ColinAdo <colinadodev30@gmail.com>
2024-10-04 13:01:24 +0300

    Add redux toolkit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-// import Provider from '@/redux/provider';
+import Provider from "@/redux/provider";
 // import { Footer, Navbar } from '@/components/common';
 // import { Setup } from '@/components/utils';
 
@@ -20,14 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Provider>
-					<Setup />
+        <Provider>
+          {/* <Setup />
 					<Navbar /> */}
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
-          {children}
-        </div>
-        {/* <Footer />
-				</Provider> */}
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
+            {children}
+          </div>
+          {/* <Footer /> */}
+        </Provider>
       </body>
     </html>
   );
