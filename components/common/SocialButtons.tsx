f5cae69fc88374f9dd2907a4bfbc9a89a1aca66f
ColinAdo <colinadodev30@gmail.com>
2024-10-04 13:39:24 +0300

    Add complete Oauth code

--- components/common/SocialButtons.tsx
+++ components/common/SocialButtons.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { ImGoogle, ImFacebook } from 'react-icons/im';
-import { SocialButton } from '@/components/common';
-import { continueWithGoogle, continueWithFacebook } from '@/utils';
+import { ImGoogle, ImGithub } from "react-icons/im";
+import { SocialButton } from "@/components/common";
+import { continueWithGoogle, continueWithGithub } from "@/utils";
 
 export default function SocialButtons() {
-	return (
-		<div className='flex justify-between items-center gap-2 mt-5'>
-			<SocialButton provider='google' onClick={continueWithGoogle}>
-				<ImGoogle className='mr-3' /> Google Signin
-			</SocialButton>
-			<SocialButton provider='facebook' onClick={continueWithFacebook}>
-				<ImFacebook className='mr-3' /> Facebook Signin
-			</SocialButton>
-		</div>
-	);
+  return (
+    <div className="flex justify-between items-center gap-2 mt-5">
+      <SocialButton provider="google" onClick={continueWithGoogle}>
+        <ImGoogle className="mr-3" /> Google Signin
+      </SocialButton>
+      <SocialButton provider="github" onClick={continueWithGithub}>
+        <ImGithub className="mr-3" /> Github Signin
+      </SocialButton>
+    </div>
+  );
 }
