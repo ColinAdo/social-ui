2e226041a3ad37a1361743f0d44d4ce1c1954227
ColinAdo <colinadodev30@gmail.com>
2024-10-04 13:08:14 +0300

    Add utils hooks and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import Provider from "@/redux/provider";
-// import { Footer, Navbar } from '@/components/common';
-// import { Setup } from '@/components/utils';
+import { Footer, Navbar } from "@/components/common";
+import { Setup } from "@/components/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,12 +21,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-          {/* <Setup />
-					<Navbar /> */}
+          <Setup />
+          <Navbar />
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
             {children}
           </div>
-          {/* <Footer /> */}
+          <Footer />
         </Provider>
       </body>
     </html>
